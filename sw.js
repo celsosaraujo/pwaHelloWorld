501c8dcba5caee5e93d895ee9c4914fe00f0b8da
celso <celso@sofolha.com.br>
2023-03-09 15:54:00 -0300

    Publicação do projeto

---
+++ sw.js
@@ -0,0 +1,26 @@
+var cacheName = 'ola-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js'
+];
+
+/* Inicia o Service Worker e armazene em cache o conteúdo do aplicativo*/
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+/* Servidor de armazenamento de cache quando estiver offline*/ 
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
