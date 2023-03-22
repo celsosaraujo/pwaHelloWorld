3bad0ce11adef471de49aec46182dfe94fdd787c
Celso Serrano Araujo <celso.saraujo@hotmail.com>
2023-03-21 21:50:56 -0300

    Tratamento da notificação para apresentar na tela

--- sw.js
+++ sw.js
@@ -8,30 +8,34 @@ var filesToCache = [
 ];
 
 /* Inicia o Service Worker e armazene em cache o conteúdo do aplicativo*/
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
     })
   );
   self.skipWaiting();
 });
 
-/* Servidor de armazenamento de cache quando estiver offline*/ 
-self.addEventListener('fetch', function(e) {
+/* Servidor de armazenamento de cache quando estiver offline*/
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
 });
 
 self.addEventListener('push', event => {
-  console.log('Push received:', event);
+  const data = event.data.json();
+  console.log('Push received:', data);
+
+  const options = {
+    body: data.body,
+    icon: 'icon.png'
+  };
+
   event.waitUntil(
-    self.registration.showNotification('Título da notificação', {
-      body: data.body,
-      icon: 'icon.png'
-    })
+    self.registration.showNotification(data.title, options)
   );
 });
